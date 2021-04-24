4a3afc735cdac4206fcdfec0da9c89c7447544c8
Blagovest Todorov <72020361+Blagovest-Todorov@users.noreply.github.com>
2021-04-24 09:12:31 +0300

    publish the correct solution to the task

--- Program.cs
+++ Program.cs
@@ -1,30 +1,68 @@
-﻿using System;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
-namespace LambdaExp
+namespace Delegates
 {
     class Program
     {
         static void Main(string[] args)
-        {   // Lambda expresssons anonymous Functions, inline functions, they replace the normal methods to write
-            int[] array = Console.ReadLine()  // no name functions, give built in functionality
-                .Split(" ", StringSplitOptions  // provide better readable code
-                .RemoveEmptyEntries)
+        {
+            int[] intervalNums = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
                 .Select(int.Parse)
                 .ToArray();
-
-            int[] evenNumbers = array
-                .Where((int x,int index) =>
-               {
-                   Console.WriteLine($"Checking at Array[{index}] {x} % 2 == 0 -> {x % 2 == 0}");
-                   return x % 2 == 0 && x > 0;  // Predicate- function that returns yes or no
-                })
-                .Where(x => x > 0)
-                .Where(x => x % 10 == 0)
-                .ToArray();
-
-            Console.WriteLine(string.Join(" ", evenNumbers));
             
-        }
+            int startNumber = intervalNums[0];
+            int endNumber = intervalNums[1];
+            string criteria = Console.ReadLine();
+
+            Func<int, int, List<int>> generateRangeOfNums = (s, e) =>
+            {
+                 List<int> nums = new List<int>();
+
+                 for (int i = s; i <= e; i++)
+                 {
+                     nums.Add(i);
+                 }
+
+                 return nums;
+            };
+
+            List<int> numbers = generateRangeOfNums(startNumber, endNumber);
+
+            Predicate<int> predicate = n => true; // (n % 2 == 0) => true;   // Predicate-> function  that returns bool value -> true or  false        
+
+            if (criteria == "odd")
+            {
+                predicate = n => n % 2 != 0;
+            }
+            else if (criteria == "even")
+            {
+                predicate = n => n % 2 == 0;
+            }
+
+            //MyWhere(numbers, n => n % 2 == 0);
+            ///MyWhere(numbers, predicate);
+            Console.WriteLine(string.Join(" ", MyWhere(numbers, predicate)));
+
+            static List<int> MyWhere(List<int> numbers, Predicate<int> predicate) 
+            {
+                List<int> newNumbers = new List<int>();
+
+                foreach (var currNum in numbers)
+                {
+                    if (predicate(currNum))
+                    {
+                        newNumbers.Add(currNum);
+                    }
+                }
+
+                return newNumbers;                
+            }
+        }        
     }
 }
+
